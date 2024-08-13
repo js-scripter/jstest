6cafe82e61395613b28212abef1e0a926066f704
Sachin <sachin.noder@gmail.com>
2024-08-13 22:15:48 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -8,20 +8,6 @@ module.exports.removeDuplicates = function(array){
 
 module.exports.sumToTarget = function(array , x) {
 
-    for (i = 0; i < (array.length - 1); i++) {
-        for (j = (i + 1); j < array.length; j++) {
-    
-            if (array[i] + array[j] == x) {
-                // console.log("Pair with a given sum " + x + " is (" + array[i] + ", " + array[j] + ")");
-                let output = []
-                output.push(array[i])
-                output.push(array[j])
-                return output
-            }
-        }
-    }
-
-    return [];
 }
 
 
